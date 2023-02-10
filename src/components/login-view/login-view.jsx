c500c7d20dd757853d44f4ec29ba31df7d357a28
shivg90 <siobhan.gatenby1@live.co.uk>
2023-02-10 13:05:37 +0100

    imported Form and Button

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState("");
