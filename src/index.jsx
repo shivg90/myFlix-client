04f392a68ba7834b67a770d57e3d4f6c27135e30
shivg90 <siobhan.gatenby1@live.co.uk>
2023-02-11 12:50:10 +0100

    removed bootstrap.min import

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,6 @@ import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import Container from "react-bootstrap/Container";
 
-// import bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-
-
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
