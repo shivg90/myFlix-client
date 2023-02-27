f2431564b1d8bb8e77a99f501774e9e7d286da67
shivg90 <siobhan.gatenby1@live.co.uk>
2023-02-27 14:27:53 +0100

    added comments to new code

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,22 +5,23 @@ import { Link } from "react-router-dom";
 import './movie-card.scss';
 
 
-export const MovieCard = ({movie }) => {
+export const MovieCard = ({ movie }) => {
   
   
   const token = localStorage.getItem('token');
   const user = JSON.parse(localStorage.getItem('user'));
   console.log("user", user);
 
-
-  //check if the movie is already in the favorite list of the user
+  // .find method checks if there is a movie in the users Favorite Movies array by id
   const alreadyFavorite = user.FavoriteMovies.find(id => id === movie.id);
   
+  // useState checks the returned value of alreadyFavorite, if a value is returned "true", if not "false"
   //console.log ("Favorite ?", alreadyFavorite);
   const [favorite, setFavorite] = useState(alreadyFavorite? true : false);
   
   //console.log(favorite);
   const toggleFavorite = () => {
+    // if token is undefined/empty, the return statement stops the execution of the function
     if (!token) return;
 
     const url = `https://movieapi-9rx2.onrender.com/users/${user.Username}/movies/${movie.id}`;
@@ -32,23 +33,27 @@ export const MovieCard = ({movie }) => {
       }
     };
   
+    // combine add and remove fav under same request options header with if else statement
+    // use different .methods for adding or removing
+    // use the different states of setFavorite (false to remove movie, true to add movie)
     if (alreadyFavorite) {
       requestOptions.method = 'DELETE';
-      alert("Movie deleted from your favorite list");
+      alert("Movie deleted from favorites!");
       setFavorite (false);
     } else {
       requestOptions.method = 'POST';
-      alert("Movie added from your favorite list");
+      alert("Movie added to favorites!");
       setFavorite (true);
     }
 
+    // return data
     fetch(url, requestOptions)
       .then((response) => response.json())
       .then((data) => {
         //setFavorite = (!alreadyFavorite);
         //console.log ("data", data);
         //console.log("user post favorite clic", user);
-        localStorage.setItem('user', JSON.stringify(data));
+        localStorage.setItem('user', JSON.stringify(data)); // sets new movie data to the user's local storage
         //window.location.reload(false);
       })
       .catch((e) => {
@@ -57,7 +62,7 @@ export const MovieCard = ({movie }) => {
 
   };
 
-  console.log("user post favorite clic 2", user);
+  console.log("user post favorite click 2", user);
 
   return (
     <Card className="h-100" style={{marginTop: 20, backgroundColor: "whitesmoke"}}>
